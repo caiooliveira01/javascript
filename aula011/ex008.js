fc69a663ec3036a1aa79d6601b865780df577d5a
caiooliveira01 <122302892+caiooliveira01@users.noreply.github.com>
2023-02-17 13:19:09 -0300

    Condições em JS

---
+++ aula011/ex008.js
@@ -0,0 +1,9 @@
+var vel = 150
+var lim = 110
+
+if (vel > lim) { // Condição SIMPLES
+    console.log(`Você dirigiu acima do limite de velocidade ${lim}Km/h`)
+    console.log('Multado, mais cuidado da próxima vez!')
+}
+
+console.log('Dirija sempre usando cinto de segurança!')
---
+++ aula011/ex009.js
@@ -0,0 +1,7 @@
+var país = 'França'
+console.log(`Cidadania referente à(aos) ${país}`)
+if (país == 'Brasil') {
+    console.log('Você é Brasileiro')
+} else {
+    console.log('Você é Estrangeiro')
+}
