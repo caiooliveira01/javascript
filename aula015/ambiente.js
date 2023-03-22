f7f956092b82379e3d65907ce359b3e96a6f2916
caiooliveira01 <122302892+caiooliveira01@users.noreply.github.com>
2023-03-22 15:20:56 -0300

    aula015

---
+++ aula015/ambiente.js
@@ -0,0 +1,8 @@
+let num = [5, 8, 2, 9, 3]
+num[3] = 6 // Valor do elemento 3
+num.push(7) // Acresenta ao final do array
+num.sort() // Organiza em ordem crescente
+
+console.log(`Nosso vetor é o ${num}`)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
---
+++ aula015/vetornatela.js
@@ -0,0 +1,11 @@
+let valores = [5, 2, 9,]
+valores.sort()
+valores.push(6)
+
+for (let pos = 0; pos < valores.length; pos++) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
+
+for (let pos in valores) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
