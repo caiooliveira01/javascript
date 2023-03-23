405d246a3c8b10a9a8b1bfb64bc9a4c71566cfcf
caiooliveira01 <122302892+caiooliveira01@users.noreply.github.com>
2023-03-23 16:21:04 -0300

    Funções

--- aula015/ambiente.js
+++ aula015/ambiente.js
@@ -6,3 +6,10 @@ num.sort() // Organiza em ordem crescente
 console.log(`Nosso vetor é o ${num}`)
 console.log(`O vetor tem ${num.length} posições`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+let pos = num.indexOf(4) // Busca de valores dentro do array
+if (pos == -1) {
+    console.log("O valor não foi encontrado")
+} else {
+    console.log(`O valor 8 está na posição ${pos}`)
+}
